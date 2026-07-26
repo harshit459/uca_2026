96439c63c346590ff9bfab55d0417e751ed88fe2
harshit459 <harshit12063@gmail.com>
2026-07-26 14:57:24 +0530

    Added web programming ques

---
+++ week_3_assign/web_programming/apiDetails_console/main.js
@@ -0,0 +1,9 @@
+var worker = new Worker("worker.js");
+
+worker.onmessage = function(event) {
+    console.log(event.data);
+};
+
+worker.onerror = function() {
+    console.log("Worker Error");
+};
