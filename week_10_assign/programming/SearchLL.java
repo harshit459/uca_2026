a998306f44884acb8ff641c764ee9adf770e57ac
harshit459 <harshit12063@gmail.com>
2026-09-23 18:56:07 +0530

    added Search in linked list ques

--- week_10_assign/programming/SearchLL.java
+++ week_10_assign/programming/SearchLL.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
 public class SearchLL {
-        static class ListNode {
-                int data;
-                ListNode next;
+	static class ListNode {
+		int data;
+		ListNode next;
 
-                ListNode(int data) {
-                        this.data = data;
-                        this.next = null;
-                }
-        }
+		ListNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
 
-        static boolean search(ListNode head1, ListNode head2) {
+	static boolean search(ListNode head1, ListNode head2) {
 
 		if(head1 == null)
 			return true;
 
 		while(head2 != null){
-		
+
 			ListNode temp1 = head1;
 			ListNode temp2 = head2;
 
@@ -34,23 +34,23 @@ public class SearchLL {
 		return false;
 	}
 
-        public static void main(String[] args) {
+	public static void main(String[] args) {
 
-                ListNode head1 = new ListNode(1);
-                head1.next = new ListNode(2);
+		ListNode head1 = new ListNode(1);
+		head1.next = new ListNode(2);
 
 		ListNode head2 = new ListNode(1);
 		head2.next = new ListNode(3);
 		head2.next.next = new ListNode(1);
 		head2.next.next.next = new ListNode(3);
-                head2.next.next.next.next = new ListNode(3);
-                head2.next.next.next.next.next = new ListNode(4);
+		head2.next.next.next.next = new ListNode(3);
+		head2.next.next.next.next.next = new ListNode(4);
 
-                
+
 		boolean result = search(head1, head2);
 
-                System.out.println(result);
+		System.out.println(result);
 
-        }
+	}
 }
 
