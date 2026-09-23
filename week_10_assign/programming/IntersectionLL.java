e8d32a373ad21784f7b68f461498c0385918b094
harshit459 <harshit12063@gmail.com>
2026-09-23 18:42:22 +0530

    added intersection of linked list ques

--- week_10_assign/programming/IntersectionLL.java
+++ week_10_assign/programming/IntersectionLL.java
@@ -1,25 +1,25 @@
 import java.util.*;
 
 public class IntersectionLL {
-        static class ListNode {
-                int data;
-                ListNode next;
+	static class ListNode {
+		int data;
+		ListNode next;
 
-                ListNode(int data) {
-                        this.data = data;
-                        this.next = null;
-                }
-        }
+		ListNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
 
-        static ListNode intersection(ListNode head1, ListNode head2) {
-	
+	static ListNode intersection(ListNode head1, ListNode head2) {
+
 		ListNode resultHead = null;
 		ListNode resultTail = null;
 
 		while(head1 != null && head2 != null) {
-		
+
 			if(head1.data == head2.data){
-				
+
 				ListNode newNode = new ListNode(head1.data);
 				if(resultHead == null){
 					resultHead = newNode;
@@ -37,47 +37,47 @@ public class IntersectionLL {
 				head1 = head1.next;
 			else
 				head2 = head2.next;
-		
+
 		}
 
 		return resultHead;
-	
+
 	}
 
-        static void printList(ListNode head) {
-                while(head != null) {
+	static void printList(ListNode head) {
+		while(head != null) {
 
-                        System.out.print(head.data);
+			System.out.print(head.data);
 
-                        if(head.next != null)
-                                System.out.print("->");
+			if(head.next != null)
+				System.out.print("->");
 
-                        head = head.next;
-                }
-                System.out.println();
-        }
+			head = head.next;
+		}
+		System.out.println();
+	}
 
-        public static void main(String[] args) {
+	public static void main(String[] args) {
 
-                ListNode head1 = new ListNode(1);
-                head1.next = new ListNode(2);
-                head1.next.next = new ListNode(2);
-                head1.next.next.next = new ListNode(3);
+		ListNode head1 = new ListNode(1);
+		head1.next = new ListNode(2);
+		head1.next.next = new ListNode(2);
+		head1.next.next.next = new ListNode(3);
 		head1.next.next.next.next = new ListNode(4);
 
 		ListNode head2 = new ListNode(2);
-                head2.next = new ListNode(2);
-                head2.next.next = new ListNode(2);
-                head2.next.next.next = new ListNode(4);
+		head2.next = new ListNode(2);
+		head2.next.next = new ListNode(2);
+		head2.next.next.next = new ListNode(4);
 
-                System.out.println("Original Linked List: ");
-                printList(head1);
+		System.out.println("Original Linked List: ");
+		printList(head1);
 		printList(head2);
 
-                ListNode result = intersection(head1, head2);
+		ListNode result = intersection(head1, head2);
 
-                System.out.println("Intersected Linked List: ");
-                printList(result);
+		System.out.println("Intersected Linked List: ");
+		printList(result);
 
-        }
+	}
 }
